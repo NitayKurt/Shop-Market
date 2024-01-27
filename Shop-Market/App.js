6819175d3b0b90cdc3bb07368e5296a096cae72d
Nitay Kurt <nitay121@gmail.com>
2024-01-27 21:51:53 +0200

    update packages

--- Shop-Market/App.js
+++ Shop-Market/App.js
@@ -2,11 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
+
+  const [editor, setEditor] = useState('');
+
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" backgroundColor='#1E92C4'/>
     <View>
       <Text>My Shop Market List</Text>
+      <Text>Last edit by: {editor}</Text>
     </View>
     </SafeAreaView>
   );
