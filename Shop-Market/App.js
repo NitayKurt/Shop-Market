93d4a3028ae067f9eb22fe8cdf3302cbd009989c
Nitay Kurt <nitay121@gmail.com>
2024-01-27 22:55:52 +0200

    try

--- Shop-Market/App.js
+++ Shop-Market/App.js
@@ -5,15 +5,35 @@ import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 export default function App() {
 
   const [editor, setEditor] = useState('');
+  const [items, setItems] = useState([]);
+
+  const addItem = () => {
+    setItems([...items, 'Item ' + (items.length + 1)]);
+  };
+
+  const removeItem = () => {
+    setItems(items.slice(0, -1));
+  };
 
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" backgroundColor='#1E92C4'/>
+
         <View>
-          <Text>My Shop Market List</Text>
+          <Text>My Shop Market List🛒</Text>
           <Text>Last edit by: {editor}</Text>
         </View>
+
+
+      {items?.map((item, index) => (
+        <View key={index}>
+          <Text>{item}</Text>
+        </View>
+      ))}
+     
+      
+
     </SafeAreaView>
   );
 }
