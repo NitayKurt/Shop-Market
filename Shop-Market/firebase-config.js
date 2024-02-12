5009f204412658e2f120357c4825f1ac35df05f5
Nitay Kurt <nitay121@gmail.com>
2024-02-12 19:47:27 +0200

    ENV UPDATE

--- Shop-Market/firebase-config.js
+++ Shop-Market/firebase-config.js
@@ -1,19 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import {API_KEY, AUTH_DOMAIN , PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID,MEASUREMENT_ID} from '@env';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAqPVrLVJ1VfrHt2A4dSrpUip8sJYb0Rns",
-  authDomain: "shop-market-20c07.firebaseapp.com",
-  projectId: "shop-market-20c07",
-  storageBucket: "shop-market-20c07.appspot.com",
-  messagingSenderId: "43241391136",
-  appId: "1:43241391136:web:03ae73d713b346cb0c6845",
-  measurementId: "G-5J9ESMTC0M"
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID
 };
 
 // Initialize Firebase
