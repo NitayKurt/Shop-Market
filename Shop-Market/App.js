671273fe0c713f88b8b58b6aed34dcf72532b872
Nitay Kurt <nitay121@gmail.com>
2024-05-15 12:57:09 +0300

    df

--- Shop-Market/App.js
+++ Shop-Market/App.js
@@ -1,12 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { StyleSheet,} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from './screens/Login';
+import ShopList from './screens/ShopList';
+import { StatusBar } from 'expo-status-bar';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View>
-      <Text>Ap2p</Text>
-    </View>
-  )
-}
 
-const styles = StyleSheet.create({})
+  const [user, setUser] = useState('');
+
+  return (
+    <NavigationContainer>
+      <StatusBar backgroundColor="#82BDC1" />
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false, // Hide header
+          cardStyle: { backgroundColor: '#82BDC1' }, // Set background color for screens
+        }}>
+        {user ? (
+          <Stack.Group screenOptions={{ headerShown: false }}>
+            <Stack.Screen name={'ShopList'} component={ShopList} />
+          </Stack.Group>
+        ) : (
+            <Stack.Group screenOptions={{ headerShown: false }}>
+              <Stack.Screen name={'Login'}>
+                {(props) => <Login {...props} setUser={setUser} />}
+              </Stack.Screen>
+            </Stack.Group>
+          )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+};
+
+const styles = StyleSheet.create({
+
+});
