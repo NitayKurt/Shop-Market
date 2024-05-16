c534d1f33a1d2c844d98e83cf2a2d78e64510577
Nitay Kurt <nitay121@gmail.com>
2024-05-16 19:58:33 +0300

    update

--- Shop-Market/screens/ShopList.js
+++ Shop-Market/screens/ShopList.js
@@ -4,12 +4,15 @@ import { StyleSheet, Text, View, SafeAreaView, TextInput, TouchableOpacity, Scro
 import { Card, Button } from 'react-native-paper';
 import EmptyListCase from '../components/EmptyListCase';
 import { collection, getDocs, addDoc } from 'firebase/firestore';
-import { db, addData, } from '../firebase-config';
-
+import { FIRESTORE_DB, } from '../firebase-config';
+import { FontAwesome } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function ShopList() {
 
-  const [editor, setEditor] = useState('ofri');
+  const [lastEditor, setLastEditor] = useState('');
+  const [lastEditAt, setLastEditAt] = useState('');
+  const [currentEditor, setCurrentEditor] = useState('ofri');
   const date = new Date().toLocaleString();
   const [item, setItem] = useState('');
   const [items, setItems] = useState([]);
@@ -18,7 +21,7 @@ export default function ShopList() {
 
   const getData = async () => {
     try {
-      const querySnapshot = await getDocs(collection(db, "market","products"));
+      const querySnapshot = await getDocs(collection(FIRESTORE_DB, "products"));
       setData(querySnapshot);
       console.log(data);
     } catch (error) {
@@ -38,8 +41,6 @@ export default function ShopList() {
       if(!items.includes(item)){
       setItems([...items, item]);
       setItem(''); // Clear the input after saving
-      // collection(database, "market-list").add({ item: item, editor: editor, date: date });
-      // await addDoc(itemsRef, { item: item, editor: editor, date: date, items: items});
       
     }
     else{
@@ -58,14 +59,17 @@ export default function ShopList() {
 
 
   const sendList = async () => {
-   
-
-      const docRef = addDoc(collection(db, "marketList"), {
-        title: "Ada",
-        last: "Lovelace",
+    try {
+      const docRef = await addDoc(collection(FIRESTORE_DB, 'products'), {
+        items: items,
+        editor: currentEditor,
+        date: date,
       });
-      console.log("Document written with ID: ",docRef);
-   
+      console.log("Document written with ID: ",docRef.id);
+    } catch (error) {
+      console.error('Error adding document: ', error);
+    }
+    
   };
 
   const deleteItem = (index) => {
@@ -93,16 +97,16 @@ export default function ShopList() {
 
       <StatusBar style="auto" backgroundColor="#82BDC1" />
 
-      <ScrollView>
+      
 
       <View style={styles.header}>
 
         <Text style={{ fontSize: 20, fontWeight: 'bold', margin: 10 }}>רשימת הקניות שלי🛒</Text>
 
-        <Text style={{ fontSize: 20 }}>Last edit by: {editor} AT: {date}</Text>
+        <Text style={{ fontSize: 18 }}>Last edit by: {currentEditor} AT: {date}</Text>
 
         <TextInput
-          style={{ height: 40 , color: '#1E92C4', borderColor: '#1E92C4', borderWidth: 1, borderRadius: 5, padding: 10, margin: 20,}}
+          style={{ height: 40 , color: '#1E92C4', borderColor: '#1E92C4', borderWidth: 2, borderRadius: 5, padding: 10, margin: 20, backgroundColor: 'white'}}
           placeholder="הקש כאן כדי להוסיף דברים לרשימה"
           onChangeText={(text) => setItem(text)}
           value={item}
@@ -114,10 +118,14 @@ export default function ShopList() {
 
       {/* If the list is empty display this just for nice UI*/}
       {items.length === 0 && (
+        <View style={{ position:"absolute" ,alignSelf:"center", top:300}}>
           <EmptyListCase />
+        </View>
       )}
-        
+                
 
+      <View style={ items.length > 0 ? styles.itemsList : styles.itemsListEmpty}>
+        <ScrollView>
       {/* Display the list as card with option to delete */}
         {items.slice().reverse().map((item, index) => (
           <Card key={index} style={{ backgroundColor: "white" , borderColor:"#1E92C4", borderWidth:1}}>
@@ -131,22 +139,23 @@ export default function ShopList() {
             </Card.Actions>
           </Card>
         ))}
+        </ScrollView>
+      </View>
 
+     
 
-      </ScrollView>
-
-      <View style={{flexDirection:"row",justifyContent: 'space-between'}}>
+      <View style={styles.buttonsSection}>
 
         <TouchableOpacity onPress={() => sendList()}>
-          <Text style={styles.saveBotton}>Save</Text>
+          <FontAwesome style={styles.saveBotton} name="send" size={24} color="black" />
         </TouchableOpacity>
 
         <TouchableOpacity onPress={() => getData()}>
-        <Text style={styles.refershBotton}>Refresh</Text>
+        <FontAwesome style={styles.refershBotton} name="refresh" size={24} color="black" />
         </TouchableOpacity>
 
         <TouchableOpacity onPress={() => clearList()}>
-          <Text style={styles.clearBotton}>Clear List</Text>
+          <AntDesign style={styles.clearBotton} name="delete" size={24} color="black" />
         </TouchableOpacity>
 
       </View>
@@ -159,14 +168,15 @@ export default function ShopList() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     justifyContent: 'flex-start',
     paddingTop: 35,
+    borderColor: '#82BDC1',
+    borderWidth:5,
   },
   header: {
     flex: 1,
     alignItems: 'center',  // Updated this line
-    backgroundColor: '#fff',
+    backgroundColor: '#82BDC1',
     padding: 10,
   },
   item: {
@@ -200,5 +210,20 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     alignContent: "flex-end",
     right: 0,
+  },
+  itemsList:{
+    borderColor: '#1E92C4',
+    borderWidth: 5,
+    flex: 1,
+    borderRadius: 15,
+   
+  },
+  itemsListEmpty:{
+   
+  },
+  buttonsSection:{
+    flexDirection:"row",
+    justifyContent: 'space-between',
+    backgroundColor: '#82BDC1',
   },
 });
