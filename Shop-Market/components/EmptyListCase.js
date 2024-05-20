2492ca010e7e5ba88c184c99788ba97bd4576d6a
Nitay Kurt <nitay121@gmail.com>
2024-05-20 12:03:10 +0300

    emptylistcase fix style

--- Shop-Market/components/EmptyListCase.js
+++ Shop-Market/components/EmptyListCase.js
@@ -3,11 +3,22 @@ import React from 'react'
 
 export default function EmptyListCase() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text style={{ fontSize: 20, fontWeight: 'bold', textAlign: 'center' }}>הרשימה ריקה כרגע</Text>
+    <View style={styles.container}>
+    <Text style={styles.text}>הרשימה ריקה כרגע</Text>
     <Image source={require('../assets/marketCart.png')} style={{ width: 200, height: 200 }} />
   </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+})
