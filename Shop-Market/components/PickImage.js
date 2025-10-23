4a699ac295b1f9106129145f8a39b1df4f661371
Nitay Kurt <nitay121@gmail.com>
2025-10-23 15:09:20 +0300

    Add frozen products image and update category title in JSON

--- Shop-Market/components/PickImage.js
+++ Shop-Market/components/PickImage.js
@@ -1,27 +1,17 @@
-export default function PickImage(imageName) {
-  
-  switch (imageName) {
-    case 'dairy_products':
-      return require('../assets/milk.png');
-    case 'fruits_and_vegetables':
-      return require('../assets/fruits_and_vegetables.png');
-    case 'canned_goods':
-      return require('../assets/canned_goods.png');
-    case 'salads':
-      return require('../assets/salads.png');
-    case 'meat_and_fish':
-      return require('../assets/meat_and_fish.png');
-    case 'general_products':
-      return require('../assets/general_products.png');
-    case 'drinks':
-      return require('../assets/drinks.png');
-    case 'cleaning_products_and_disposables':
-      return require('../assets/cleaning_products_and_disposables.png');
-    case 'personal_care_and_hygiene':
-      return require('../assets/personal_care_and_hygiene.png');
-    case 'nuts':
-      return require('../assets/nuts.png');
-    default:
-      return require('../assets/general_products.png');
-  }
+const categoryImages = {
+  'מוצרי חלב': require('../assets/milk.png'),
+  'פירות וירקות': require('../assets/fruits_and_vegetables.png'),
+  'שימורים': require('../assets/canned_goods.png'),
+  'סלטים וממרחים': require('../assets/salads.png'),
+  'דגים ובשר': require('../assets/meat_and_fish.png'),
+  'מוצרים כלליים': require('../assets/general_products.png'),
+  'שתייה': require('../assets/drinks.png'),
+  'חומרי ניקוי וחד פעמי': require('../assets/cleaning_products_and_disposables.png'),
+  'טיפוח והיגיינה אישית': require('../assets/personal_care_and_hygiene.png'),
+  'אגוזים': require('../assets/nuts.png'),
+  'קפואים': require('../assets/frozen_products.png'),
+};
+
+export default function PickImage(hebrewTitle) {
+  return categoryImages[hebrewTitle] || require('../assets/general_products.png');
 };
